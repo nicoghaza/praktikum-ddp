a4e46cdbbe63fd3b3ab96c5841a16bbd66456024
nicoghaza <114562908+nicoghaza@users.noreply.github.com>
2022-12-18 16:50:00 +0700

    Add files via upload

---
+++ Tugas_OOP_0110122041_NicoGhazaArdian_SI10_Senin/gempa.py
@@ -0,0 +1,24 @@
+class gempa :
+    #kasih variabel
+    lokasi = ""
+    skala = 0
+    #kasih konstruktor
+    def __init__(self,lokasi,skala):
+        self.lokasi = lokasi
+        self.skala = skala
+    #member3 method
+    def dampak (self):
+        if(self.skala < 2 ) :
+            ket = "tidak terasa"
+        elif(self.skala >= 2 and self.skala < 4) :
+            ket = "Bangunan retak-retak"
+        elif(self.skala >= 4 and self.skala < 6) :
+            ket = "Bangunan roboh"
+        else :
+            ket = "Berpotensi tsunami"
+        print(
+            "Lokasi Gempa\t:",self.lokasi,
+            "\nSkala\t\t\t:",self.skala,"richter"
+            "\nDampak\t\t\t:",ket,
+            "\n-------------------------------------"
+        )
